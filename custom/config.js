ab8dcf933ee182cfb55eb71d1164df32d614b80b
NoCtrlZ <18020651@vnu.edu.vn>
2020-06-08 09:03:05 +0700

    Add config

--- custom/config.js
+++ custom/config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    APP_NAME: "uet-getnews",
+  APP_NAME: "uet-getnews",
 
-    REPORT_LINK: "https://www.google.com.vn/",
+  REPORT_LINK: "https://www.google.com.vn/",
 
-    // FACEBOOK
-    FB_PAGE_ACCESS_TOKEN: "",
-    FB_PAGE_VERIFY_TOKEN: "",
-    FB_APP_SECRET: "",
+  // FACEBOOK
+  FB_PAGE_ACCESS_TOKEN:
+    "EAAItZAekgTxABALMZCNmI8GkGzDYbEL8BZCUrskxzsNHM5JFcPCLhUTYAlvyLHOC0tk4zoP0ZAWcoNQP6xEdQ11Vihu879Es6r0IPP0s3ZBAQOjOJ7zF5GZCkJmlGbPsoGxsq7ZB3Ge56NuqdxXSInQaN38TIkWfZBw65qhDuc1L1SFbi7qZAIquqVv2Nwex3sJoZD",
+  FB_PAGE_VERIFY_TOKEN: "noctrlz",
+  FB_APP_SECRET: "ea2fa77518713380f9777fb3feee4cb8",
 
-    //MONGODB
-    MONGO_URI: "",
+  //MONGODB
+  MONGO_URI:
+    "mongodb+srv://admin:phonglon@cluster1-ah9vf.mongodb.net/test?retryWrites=true&w=majority",
 };
